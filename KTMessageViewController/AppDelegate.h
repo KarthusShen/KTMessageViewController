1f133f0df2cf1388c398b1e73786858b27a89c07
ShenHaoWen <805154269@qq.com>
2017-10-11 10:38:28 +0800

    Initial Commit

---
+++ KTMessageViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KTMessageViewController
+//
+//  Created by 沈浩文 on 2017/10/11.
+//  Copyright © 2017年 karthus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ KTMessageViewController/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  KTMessageViewController
+//
+//  Created by 沈浩文 on 2017/10/11.
+//  Copyright © 2017年 karthus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
