cabd4161c0323d2a100687fe7d31842973220efb
KarthusShen <shw0418@126.com>
2017-10-15 11:18:20 +0800

    KTMessagesCollectionViewCell
    I just define this class in .h file, havn't implement the .m file since it relies on some other classes that not done yet

---
+++ KTMessageViewController/Views/KTMessagesCollectionViewCell.m
@@ -0,0 +1,22 @@
+//
+//  KTMessagesCollectionViewCell.m
+//  KTMessageViewController
+//
+//  Created by Karthus on 2017/10/15.
+//  Copyright © 2017年 karthus. All rights reserved.
+//
+
+#import "KTMessagesCollectionViewCell.h"
+
+@interface KTMessagesCollectionViewCell ()
+
+@end
+
+@implementation KTMessagesCollectionViewCell
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
