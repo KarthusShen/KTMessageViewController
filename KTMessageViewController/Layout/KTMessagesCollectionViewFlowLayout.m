03aca0726ee7cdeb5f132d92a8f65c8b4656dd22
KarthusShen <shw0418@126.com>
2017-10-15 11:10:00 +0800

    KTMessagesCollectionViewFlowLayout
    I just define this class in .h file, havn't implement the .m file since it relies on some other classes that not done yet

---
+++ KTMessageViewController/Layout/KTMessagesCollectionViewFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  KTMessagesCollectionViewFlowLayout.m
+//  KTMessageViewController
+//
+//  Created by Karthus on 2017/10/15.
+//  Copyright © 2017年 karthus. All rights reserved.
+//
+
+#import "KTMessagesCollectionViewFlowLayout.h"
+
+@implementation KTMessagesCollectionViewFlowLayout
+//TODO: implement this class
+@end
